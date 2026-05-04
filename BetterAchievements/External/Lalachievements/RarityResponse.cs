2ef8cce0432c0d20ba532a4887c88692c6d2ac6b
zodiia <git@zodiia.moe>
2026-05-04 15:01:45 +0200

    feat(lalachievements): fixed lalachievements response parsing

--- BetterAchievements/External/Lalachievements/RarityResponse.cs
+++ BetterAchievements/External/Lalachievements/RarityResponse.cs
@@ -4,4 +4,4 @@ namespace BetterAchievements.External.Lalachievements;
 
 public record RarityResponse(string CharCount, List<RarityEntry> Rarity);
 
-public record RarityEntry(uint Id, string Count, string PointsCount, double Percentile, int Points);
+public record RarityEntry(uint Id, string Count = "0", string PointsCount = "0", double Percentile = 0.0f, int Points = 0);
